f3edc88d1b9ec6c6fff66b770ba35c618f8002d7
345 <lv345_y@163.com>
2022-08-16 22:00:13 +0800

    二进制中1的个数

--- src/Solution.java
+++ src/Solution.java
@@ -7,16 +7,14 @@ import java.util.Stack;
  * @time 2022/08/09 13:56
  * @description
  */
-
-
 public class Solution {
 
 
     /**
      * 题：将二叉搜索树转为首尾相连的双向链表，返回 head 结点
      * 解：中序遍历二叉树，保存 head 结点和 pre(上一个) 结点
-     *    判断条件：pre == null，表示当前结点是 head 结点
-     *    否则和当前结点互相指向即可
+     * 判断条件：pre == null，表示当前结点是 head 结点
+     * 否则和当前结点互相指向即可
      * 时间复杂度：只需要遍历一次，O(n)
      * 空间复杂度：开辟了栈，所以也是 0(n)
      */
@@ -46,7 +44,25 @@ public class Solution {
         return head;
     }
 
-    class Node {
+
+
+    /**
+     *题： 二进制中 1 的个数，输入一个无符号整数，返回二进制表达式中数字为为 1 的个数
+     *
+     * */
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+
+
+    static class Node {
         public int val;
         public Node left;
         public Node right;
@@ -64,6 +80,4 @@ public class Solution {
             right = _right;
         }
     }
-
-    ;
 }
